9926c7852fd728a421c0eea038245d6d75742d70
JulianHohenadel <julian.hohenadel@gmail.com>
2020-05-18 11:53:30 +0200

    Exercise_03 init

---
+++ exercise_03/build/CMakeFiles/exercise_03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/exercise_03.dir/main.cpp.o"
+  "exercise_03"
+  "exercise_03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/exercise_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
