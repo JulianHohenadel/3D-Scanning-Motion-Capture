5fb15420128fe75a74d8b459fc2a3c10ece12fbf
JulianHohenadel <julian.hohenadel@gmail.com>
2020-05-22 14:37:42 +0200

    Exercise_04 init

---
+++ exercise_04/build/CMakeFiles/gaussian.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gaussian.dir/gaussian.cpp.o"
+  "gaussian"
+  "gaussian.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gaussian.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
